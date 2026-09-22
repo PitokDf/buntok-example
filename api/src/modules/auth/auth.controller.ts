35e550da268e551fb8f05835927e6880e38de7d6
Pito Desri Pauzi <108637369+PitokDf@users.noreply.github.com>
2026-09-22 15:01:15 +0700

    test zod

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,10 +1,18 @@
-import { Dependencies, Controller, Get, Context } from "@buntok/core";
+import { Dependencies, Controller, Get, Context, Use } from "@buntok/core";
 import { AuthService } from "./auth.service";
+import { z, zValidator } from "@buntok/core/middlewares/validator";
 
 @Dependencies(AuthService)
 @Controller("/auths")
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Get()
+  @Use(zValidator("query", z.object({ name: z.string().optional() })))
+  async me() {
+    return "Hello, this is the auth controller!";
+  }
+
   @Get("/login")
   async login(ctx: Context) {
     const result = await this.authService.login();
