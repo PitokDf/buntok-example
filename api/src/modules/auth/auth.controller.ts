a808685e862ea2f8cc624ea0fda2b035a670376c
Pito Desri Pauzi <108637369+PitokDf@users.noreply.github.com>
2026-09-22 15:06:05 +0700

    tambahkan import baru

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Dependencies, Controller, Get, Context, Use } from "@buntok/core";
 import { AuthService } from "./auth.service";
 import { z, zValidator } from "@buntok/core/middlewares/validator";
+import { uniq } from "@buntok/core/helpers";
+import { deleteCookie, setCookie } from "@buntok/core/helpers";
 
 @Dependencies(AuthService)
 @Controller("/auths")
@@ -10,7 +12,10 @@ export class AuthController {
   @Get()
   @Use(zValidator("query", z.object({ name: z.string().optional() })))
   async me() {
-    return "Hello, this is the auth controller!";
+    return (
+      "Hello, this is the auth controller!" +
+      uniq([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    );
   }
 
   @Get("/login")
